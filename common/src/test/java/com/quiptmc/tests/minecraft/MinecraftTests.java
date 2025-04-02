614ed941baa25c985ff9debbb94c8cb045c8a59e
QuickScythe <QuickScythe@gmail.com>
2025-04-01 22:54:30 -0500

    test fixes

--- common/src/test/java/com/quiptmc/tests/minecraft/MinecraftTests.java
+++ common/src/test/java/com/quiptmc/tests/minecraft/MinecraftTests.java
@@ -10,10 +10,6 @@ public class MinecraftTests {
 
     @Test
     void testMinecraftServerPlayer() throws MalformedURLException {
-        MinecraftServer server = new MinecraftServer.Builder("172.59.74.72", new JSONObject().put("secret", "You Should Change This To Literally Anything Else").put("callback_url", "http://localhost:5050/callback")).build();
-        server.setupApi("http://localhost:8080/api");
-        MinecraftServerPlayer player = server.apiManager().get().getPlayer(UUID.fromString("60191757-427b-421e-bee0-399465d7e852"));
-        System.out.println(player.name());
-        player.sendMessage("Hello, World!");
+
     }
 }
