b5de1fdec21a351e9cb6e08ffc75e020302bbfbc
QuickScythe <QuickScythe@gmail.com>
2025-04-21 00:47:02 -0500

    better particle registering and recalling

--- fabric/src/main/java/com/quiptmc/fabric/particles/QuiptParticle.java
+++ fabric/src/main/java/com/quiptmc/fabric/particles/QuiptParticle.java
@@ -1,9 +1,6 @@
 package com.quiptmc.fabric.particles;
 
-import com.quiptmc.minecraft.CoreUtils;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
-import net.minecraft.particle.ParticleEffect;
-import net.minecraft.particle.ParticleType;
 import net.minecraft.particle.SimpleParticleType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -11,15 +8,14 @@ import net.minecraft.util.Identifier;
 
 public class QuiptParticle {
 
-    private final static com.quiptmc.core.data.registries.Registry<ParticleType> PARTICLE_TYPE_REGISTRY = com.quiptmc.core.data.registries.Registries.register("particles", ParticleType.class);
+    private final static com.quiptmc.core.data.registries.Registry<SimpleParticleType> PARTICLE_TYPE_REGISTRY = com.quiptmc.core.data.registries.Registries.register("particles", SimpleParticleType.class);
 
-    public static void register(String key, ParticleType<? extends ParticleEffect> type) {
-        Registry.register(Registries.PARTICLE_TYPE, Identifier.of("quipt", key), type);
-        PARTICLE_TYPE_REGISTRY.register(key, type);
+    public static void register(String key) {
+        PARTICLE_TYPE_REGISTRY.register(key, Registry.register(Registries.PARTICLE_TYPE, Identifier.of("quipt", key), FabricParticleTypes.simple()));
     }
 
 
-    public static <T extends ParticleEffect> ParticleType<T> get(String key) {
+    public static SimpleParticleType get(String key) {
         return PARTICLE_TYPE_REGISTRY.get(key).orElseThrow();
     }
 
