f6788322e8519ab9b60cec31ae0dced4c92db157
QuickScythe <QuickScythe@gmail.com>
2025-04-21 20:57:03 -0500

    first item test

---
+++ fabric/src/main/java/com/quiptmc/fabric/items/TestItem.java
@@ -0,0 +1,11 @@
+package com.quiptmc.fabric.items;
+
+import com.quiptmc.fabric.items.abstracts.QuiptNormalItem;
+
+public class TestItem extends QuiptNormalItem {
+    public TestItem(Settings settings) {
+        super(settings);
+    }
+
+
+}
