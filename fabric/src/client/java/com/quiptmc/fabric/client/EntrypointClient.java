c34543f8dbf2881f36c759a31a0a7ec452a7e715
QuickScythe <QuickScythe@gmail.com>
2025-04-23 01:19:07 -0500

    Starting on food items

--- fabric/src/client/java/com/quiptmc/fabric/client/EntrypointClient.java
+++ fabric/src/client/java/com/quiptmc/fabric/client/EntrypointClient.java
@@ -2,6 +2,8 @@ package com.quiptmc.fabric.client;
 
 import com.quiptmc.fabric.blocks.QuiptBlocks;
 import com.quiptmc.fabric.particles.QuiptParticle;
+import com.quiptmc.minecraft.CoreUtils;
+import com.quiptmc.minecraft.api.ApiManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.minecraft.client.MinecraftClient;
@@ -10,14 +12,17 @@ import net.minecraft.client.particle.EndRodParticle;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.PortalParticle;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+import net.minecraft.item.Items;
 import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.world.level.storage.LevelStorage;
 
 
 public class EntrypointClient  implements ClientModInitializer {
 
+    ApiManager apiManager;
+
     @Override
     public void onInitializeClient() {
-
+        apiManager = new ApiManager(CoreUtils.integration());
     }
 }
