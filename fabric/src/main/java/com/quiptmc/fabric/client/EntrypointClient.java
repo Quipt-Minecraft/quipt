ab2a55fce729f6d3549f005b291ee574e87e6762
QuickScythe <QuickScythe@gmail.com>
2025-04-24 23:29:22 -0500

    fix ClassNotFound crash

---
+++ fabric/src/main/java/com/quiptmc/fabric/client/EntrypointClient.java
@@ -0,0 +1,12 @@
+package com.quiptmc.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+
+public class EntrypointClient  implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
