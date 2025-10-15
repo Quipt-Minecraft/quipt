c6c2fc3032afb337e1159e941f25e9f0dec4b9b3
QuickScythe <quickscythe@gmail.com>
2025-10-14 20:24:30 -0500

    Add ConfigString for string-based configuration handling

---
+++ core/src/main/java/com/quiptmc/core/config/objects/ConfigString.java
@@ -0,0 +1,26 @@
+package com.quiptmc.core.config.objects;
+
+import com.quiptmc.core.config.ConfigObject;
+import org.json.JSONObject;
+
+public class ConfigString extends ConfigObject {
+
+    String value;
+    public ConfigString(String id, String value){
+        this.value = value;
+        super.id = id;
+    }
+
+    public ConfigString(JSONObject json){
+        fromJson(json);
+    }
+
+    public String value(){
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value();
+    }
+}
