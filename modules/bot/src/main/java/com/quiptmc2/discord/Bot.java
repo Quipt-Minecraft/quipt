0497cf6b430d7002897f30f83b050a197ec6c014
QuickScythe <QuickScythe@gmail.com>
2026-02-03 21:12:26 -0600

    Introduce Bot integration with Discord: add Bot, BotConfig, BotPlugin, and BotPluginLoader classes for plugin management and configuration.

---
+++ modules/bot/src/main/java/com/quiptmc2/discord/Bot.java
@@ -0,0 +1,66 @@
+package com.quiptmc2.discord;
+
+import com.quiptmc.discord.api.QDA;
+import com.quiptmc2.discord.plugins.BotPluginLoader;
+import com.quiptmc.discord.logger.LoggerUtils;
+import com.quiptmc2.core.QuiptIntegration;
+import com.quiptmc2.discord.config.BotConfig;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+
+import java.io.File;
+
+public class Bot extends QuiptIntegration {
+    private static Bot instance = null;
+
+    private JDA jda;
+    private QDA qda;
+    private BotPluginLoader pluginLoader;
+
+    public static Bot instance(){
+        if(instance == null){
+            instance = new Bot();
+        }
+        return instance;
+    }
+    @Override
+    public String name() {
+        return "QuickBot";
+    }
+
+    @Override
+    public String version() {
+        return "1.0.0";
+    }
+
+    @Override
+    public File folder() {
+        return new File("bot-data");
+    }
+
+    @Override
+    public void enable() {
+        BotConfig config = configs().register(BotConfig.class);
+        if(config.token.equals("<YOUR_BOT_TOKEN>")){
+            logger().error("Initialization", "Bot token not set.", new ExceptionInInitializerError("Please set your bot token in the config file located at " + new File(folder(), "BotConfig.json").getAbsolutePath()));
+            LoggerUtils.log("Bot", "Please set your bot token in the config file located at: \"" + config.file().getAbsolutePath() + "\"");
+            return;
+        }
+        try {
+            JDA api = JDABuilder.createDefault(config.token, GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS).setMemberCachePolicy(MemberCachePolicy.ALL).build();
+            api.awaitReady();
+            this.jda = api;
+            this.qda = new QDA(api);
+            pluginLoader = new BotPluginLoader(this);
+            LoggerUtils.log("Bot", "Bot started successfully.");
+        } catch (InterruptedException e) {
+            LoggerUtils.log("Bot", "Error starting bot", e);
+        }
+    }
+
+    public JDA jda(){return jda;}
+    public QDA qda(){return qda;}
+    public BotPluginLoader plugins(){return pluginLoader;}
+}
