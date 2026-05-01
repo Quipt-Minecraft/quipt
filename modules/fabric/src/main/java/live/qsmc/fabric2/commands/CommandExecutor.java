59c5cf24e85856ec04c0e67ce7d361b771acadc4
Quick <quickscythe@gmail.com>
2026-05-01 02:15:53 -0500

    Implement permission management in CommandExecutor for enhanced command access control

--- modules/fabric/src/main/java/live/qsmc/fabric2/commands/CommandExecutor.java
+++ modules/fabric/src/main/java/live/qsmc/fabric2/commands/CommandExecutor.java
@@ -1,21 +1,40 @@
 package live.qsmc.fabric2.commands;
 
 import com.mojang.brigadier.arguments.ArgumentType;
-import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
+import live.qsmc.core2.Quipt;
+import live.qsmc.core2.data.registries.Registry;
 import live.qsmc.fabric2.QuiptMod;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.permission.Permission;
-import net.minecraft.command.permission.Permissions;
+import net.minecraft.command.permission.PermissionLevel;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
 
 public abstract class CommandExecutor extends Command {
 
+
+    private final Registry<Permission> permissions;
+
+
     public CommandExecutor(QuiptMod mod, String cmd) {
         super(mod, cmd);
+        permissions = Quipt.INSTANCE.registries().register("permissions", () -> null);
+    }
+
+    public Permission permission(String id){
+        if(permissions.get(id).isEmpty())
+            permissions.register(id, new Permission.Atom(Identifier.of(id)));
+        return permissions.get(id).get();
+    }
+
+    public Permission permission(int id){
+        String idStr = String.valueOf(id);
+        if(permissions.get(idStr).isEmpty())
+            permissions.register(idStr, new Permission.Level(PermissionLevel.fromLevel(id)));
+        return permissions.get(idStr).get();
     }
 
     public LiteralCommandNode<ServerCommandSource> execute() {
